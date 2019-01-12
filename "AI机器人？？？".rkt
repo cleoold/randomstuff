96e6f7cdb9f9b4e22934f96d7607a6babb3c8bfb
midori <cleoold@gmail.com>
2019-01-12 01:05:38 -0500

    Create "AI机器人？？？".rkt

---
+++ "AI机器人？？？".rkt
@@ -0,0 +1,24 @@
+#lang racket
+
+;; as what the code expects!
+
+(define (f-ai)
+  (define my-text (read-line))
+  (if
+   (let ([last-word (last (string->list my-text))])
+     (or (char=? last-word #\？) (char=? last-word #\?)))
+   ((printf "~a！\n" (substring my-text 0 (- (string-length my-text) 2)))
+    (f-ai))
+   ((printf "~a！\n" (substring my-text 0 (string-length my-text)))
+    (f-ai))))
+
+(f-ai)
+
+#| example
+> 在吗？
+在！
+> 能听懂国语吗？
+能听懂国语！
+> 真的吗？
+真的！
+|#
