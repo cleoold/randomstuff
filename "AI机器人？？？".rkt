b116a2b601b4ac66980573e1d3a34e91c3ff39b5
midori <cleoold@gmail.com>
2019-01-12 01:38:19 -0500

    Update "AI机器人？？？".rkt

--- "AI机器人？？？".rkt
+++ "AI机器人？？？".rkt
@@ -22,3 +22,20 @@
 > 真的吗？
 真的！
 |#;
+
+#|=========================================================================================
+with some aids so it is shorter!
+
+#lang racket
+
+(require racket/string)
+
+(define (f-ai)
+  (define my-text (read-line))
+  ((printf "~a\n"
+           (string-replace
+            (string-replace
+             (string-replace my-text "吗" "") "？" "！") "?" "！"))
+   (f-ai)))
+
+(f-ai)
